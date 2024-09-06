0accf3fdb6b97be2232983119faf721b38f15eba
Joaovictordevjn <joaovictor06_@hotmail.com>
2024-09-05 21:35:28 -0300

    novo site

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// VARIÁVEIS 
+// UMA FORMA DE IDENTIFICAR QUEM É QUEM
+// DÁ UM NOME PARA UM CÓD
+// É COMO SE VC CRIASSE UMA PASTA PARA GUARDAR OQ QUISER
+
+let jao = "salve"
+
+console.log
+
+// FUNÇÕES 
+// TRECHO DE CODIGO QUE SO EXECUTA QUANDO É CHAMADO
+// document igual a ir no html
+// queryselector procurar o que eu quiser
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario() {
+    formulario.style.left = "50%"
+}
+
+function desaprecerformulario() {
+    formulario.style.left = "-50%"
+}
